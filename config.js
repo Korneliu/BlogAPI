828d938949c7f910da57bf8c8504b221317a011f
Max <olbiton@gmail.com>
2017-12-17 18:51:29 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,3 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/posts';
 exports.PORT = process.env.PORT || 8000;
 
-//FirstUser:FirstUser@ds049945.mlab.com:49945/blogpost2'
